b188bd083184f6698085bde32528201835390bdc
MUSISME <mustarishafiq@gmail.com>
2026-01-18 21:06:57 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'http://127.0.0.1:5000';
 
 const App = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
